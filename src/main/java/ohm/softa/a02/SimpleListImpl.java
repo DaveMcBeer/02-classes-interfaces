386d392b24d47f7ea705499f3b3d5349e5ff5b3a
DaveMcBeer <bliemelda85101@th-nuernberg.de>
2024-01-04 17:05:23 +0100

    Lösung für 02

--- src/main/java/ohm/softa/a02/SimpleListImpl.java
+++ src/main/java/ohm/softa/a02/SimpleListImpl.java
@@ -1,11 +1,82 @@
 package ohm.softa.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList,Iterable<Object> {
 
-	// TODO: Implement the required methods.
+    private Element head;
+    private int size=0;
+    @Override
+    public Iterator<Object> iterator() {
+        return new SimpleIteratorImpl();
+    }
+
+    @Override
+    public void add(Object o) {
+        if(head == null)
+            head = new Element(o,null);
+        else {
+            Element current = head;
+            while (current.GetNext() != null) {
+                current = current.next;
+            }
+            current.next = new Element(o, null);
+        }
+        size++;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public SimpleList filter(SimpleFilter filter) {
+        SimpleList result = new SimpleListImpl();
+        for(Object o : this){
+            if(filter.include(o)){
+                result.add(o);
+            }
+        }
+        return result;
+    }
+
+    private static class Element{
+
+        Element(Object item,Element next){
+            this.item = item;
+            this.next = next;
+        }
+        private Object item;
+        public Object GetItem(){
+            return item;
+        }
+        private Element next;
+        public Element GetNext(){
+            return next;
+        }
+    }
+
+    private class SimpleIteratorImpl implements Iterator<Object>{
+
+        private Element current = head;
+
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Object next() {
+           Object temp = current.GetItem();
+           current = current.GetNext();
+           return temp;
+        }
+    }
 
 }
